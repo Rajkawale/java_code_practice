58c6a3b7d9d226f81cea8213c3dc5678b5073847
DESKTOP-UD03ICE\ADMIN <rajendra.kawale101@purplestylelabs.com>
2023-07-01 18:28:12 +0530

    commit

---
+++ src/for_loops.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.LinkedTransferQueue;
+
+public class for_loops {
+    public static void main(String[] args) {
+        for (int i=0; i<4; ){
+            System.out.println("the value of i; %d" + i );
+        }
+
+
+
+    }
+}
---
+++ src/if_condition.java
@@ -0,0 +1,10 @@
+public class if_condition {
+    public static void main(String[] args) {
+        int myNumber = 20;
+
+        if ( myNumber == 20)
+
+            System.out.println("yes");
+
+    }
+}
